0a793e1e17917b5acd9fd70bfece781ee68f3494
Henry Nawrocki <hdev@sztanski.com>
2022-05-16 20:53:51 -0500

    Add Location component | filter countries

--- src/components/Container/container.jsx
+++ src/components/Container/container.jsx
@@ -1,15 +1,40 @@
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './Container.styles.scss';
 import LocationList from '../LocationList/LocationList';
 
 const Container = (props) => {
+  const countries = useSelector((state) => state.countries);
   const { continent, country } = props;
+  if (country) {
+    return (
+      <div>
+        <LocationList location={country} type="detail" />
+      </div>
+    );
+  }
+  if (continent) {
+    const filteredCountries = countries[continent];
+    return (
+      <div className="container">
+        <div>
+          <LocationList location={continent} type="detail" />
+        </div>
+        <div>
+          <LocationList countries={filteredCountries} type="list" />
+        </div>
+      </div>
+    );
+  }
   return (
-    <div>
-      <LocationList
-        location={country || continent}
-        type={country ? 'country' : 'continent'}
-      />
-      <button type="button">Print</button>
+    <div className="continents">
+      <Link to="/AF">Africa</Link>
+      <Link to="/AS">Asia</Link>
+      <Link to="/EU">Europe</Link>
+      <Link to="/NA">North America</Link>
+      <Link to="/OC">Oceania</Link>
+      <Link to="/SA">South America</Link>
+      <Link to="/AN">Antartica</Link>
     </div>
   );
 };
--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -1,9 +1,21 @@
 import Location from '../Location/Location';
 
 const LocationList = (props) => {
-  const { location, type } = props;
+  const { location, type, countries } = props;
+  console.log(type);
+
+  if (countries) {
+    return (
+      <div className="countries-list">
+        {countries.map((country) => (
+          <Location key={country.name} location={country.name} />
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div className="header">
+    <div className={type}>
       <Location location={location} type={type} />
     </div>
   );
