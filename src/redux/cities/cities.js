31ad4c011a4dbe912b9f1650080413ff8cfb2497
Henry Nawrocki <hdev@sztanski.com>
2022-05-17 08:50:18 -0500

    Add cities reducer with createSlice

---
+++ src/redux/cities/cities.js
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getCities = createAsyncThunk(
+  'cities/getCities',
+  async (countryCode) => {
+    const res = fetch(
+      `https://wft-geo-db.p.rapidapi.com/v1/geo/cities/?countryIds=CL&limit=10`,
+      {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+          'x-rapidapi-key':
+            '5dc92c88b3mshc2c23ed3840fc2cp1390b4jsn6abe52050de3',
+        },
+      }
+    );
+    const data = await res.json();
+    return data.results;
+  }
+);
+
+const initialState = {
+  data: [],
+  status: '',
+};
+
+export default createSlice({
+  name: 'cities',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getCities.fulfilled]: (state, action) => {
+      state.data = action.payload;
+    },
+  },
+});
