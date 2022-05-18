455f54aab3653cbf69e1e77557cdf5625d9d32d3
Henry Nawrocki <hdev@sztanski.com>
2022-05-17 19:15:37 -0500

    Add search bar to nav item

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,13 +4,21 @@ const NavBar = (props) => {
   const navigate = useNavigate();
 
   const { title } = props;
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      console.log(event.target.value);
+    }
+  };
   return (
     <nav className="nav-bar">
       <button onClick={() => navigate(-1)}>{'<'}</button>
       <h1>{title}</h1>
       <div className="actions">
         <ul>
-          <li>&</li>
+          <li>
+            <input type="text" onKeyDown={handleKeyDown} />
+          </li>
         </ul>
       </div>
     </nav>
