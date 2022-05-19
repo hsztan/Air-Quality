c675d9fddbc22013c5a9136401e73434290726c7
Henry Nawrocki <hdev@sztanski.com>
2022-05-19 12:35:34 -0500

    Add a conditional to render city info

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -7,16 +7,18 @@ const City = () => {
   const city = useSelector((state) => state.city.city);
   return (
     <>
-      <NavBar title={city.name} />
-      <div className="city">
-        <h1>City</h1>
-        <p>{city.city}</p>
-        <h2>Population</h2>
-        <img src={populationImg} alt="group of people" />
-        <p>{city.population}</p>
-        <h2>Air Quality</h2>
-        <p>{city.aqi}</p>
-      </div>
+      {city ? <NavBar title={city.name} /> : null}
+      {city ? (
+        <div className="city">
+          <h1>City</h1>
+          <p>{city.name || 'N/A'}</p>
+          <h2>Population</h2>
+          <img src={populationImg} alt="group of people" />
+          <p>{city.population || 'N/A'}</p>
+          <h2>Air Quality</h2>
+          <p>{city.aqi || 'N/A'}</p>
+        </div>
+      ) : null}
     </>
   );
 };
