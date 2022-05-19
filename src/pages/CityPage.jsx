470e720294ce5da497f9052693fa28f9f9c39176
Henry Nawrocki <hdev@sztanski.com>
2022-05-19 07:46:51 -0500

    Fix style lints

--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -3,7 +3,6 @@ import City from '../components/City/City';
 
 const CityPage = () => {
   const city = useSelector((state) => state.city);
-  console.log(city);
   return <City city={city} />;
 };
 
