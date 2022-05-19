9bcaaa705d95b35c2bbdfc0e63de0346942639be
Henry Nawrocki <hdev@sztanski.com>
2022-05-18 21:55:38 -0500

    Add CityPage styles

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -1,15 +1,22 @@
+import './City.styles.scss';
+import NavBar from '../NavBar/NavBar';
+import populationImg from '../../assets/images/population.png';
 const City = (props) => {
   const { city } = props;
   console.log('city component', city);
   return (
-    <div className="city">
-      <h1>City</h1>
-      <p>{city.city.name}</p>
-      <h2>Population</h2>
-      <p>{city.city.population}</p>
-      <h2>Air Quality</h2>
-      <p>{city.city.aqi}</p>
-    </div>
+    <>
+      <NavBar title={city.city.name} />
+      <div className="city">
+        <h1>City</h1>
+        <p>{city.city.name}</p>
+        <h2>Population</h2>
+        <img src={populationImg} alt="group of people" />
+        <p>{city.city.population}</p>
+        <h2>Air Quality</h2>
+        <p>{city.city.aqi}</p>
+      </div>
+    </>
   );
 };
 
